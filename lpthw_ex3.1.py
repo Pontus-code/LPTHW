0f7115804f056c85cddbad0ddf47bd8b93d2b632
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-10-16 10:32:10 +0200

    Update lpthw_ex3.1.py

--- lpthw_ex3.1.py
+++ lpthw_ex3.1.py
@@ -1,4 +1,4 @@
-# This program will calculate the number of seconds i a day.
+# This program will calculate the number of seconds in a day.
 
 print("There are", 60 * 60 * 24, "seconds in a day!")
 
