05ca8ec788c9372811a50f4e5770716e921def43
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-11-07 10:18:02 +0100

    Add files via upload

---
+++ lpthw_ex16.py
@@ -0,0 +1,33 @@
+# Learning Python3 the Hard Way - Exercise 16. Reading and Writing Files.
+
+from sys import argv
+
+script, filename = argv
+
+print(f"We're going to erase {filename}.")
+print("If you don't want that, hit CTRL-C(^C).")
+print("If you do want that, hit RETURN.")
+
+# Prompting the user for an input. Gives user a chanse to cancel the script. The input doesn't matter. 
+input("?")
+
+print("Opening the file...")
+target = open(filename, 'w')
+
+print("Truncating the file. Goodbye!")
+target.truncate()
+
+print("Now I'm going to ask you for three lines.")
+
+line1 = input("line 1:")
+line2 = input("line 2:")
+line3 = input("line 3:")
+
+print("I'm going to write these to the file.")
+
+target.write(f"{line1}\n{line2}\n{line3}\n")
+
+print("And finally, we close it.")
+target.close()
+
+
