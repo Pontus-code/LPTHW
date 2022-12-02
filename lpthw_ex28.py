ed332d339e5fdbf60fa36d433b918923f307dd06
Pontus-code <71976575+Pontus-code@users.noreply.github.com>
2022-12-02 21:41:54 +0100

    Add files via upload

---
+++ lpthw_ex28.py
@@ -0,0 +1,64 @@
+# Leaerning Python3 the Hard Way. Excercise 28. Boolean Practice.
+
+print("1. True and True is:")
+print(True and True)
+
+print("2. False and True is:")
+print(False and True)
+
+print("3. 1 == 1 and 2 == 1 is:")
+print(1 == 1 and 2 == 1)
+
+print("4. \"test\" == \"test\" is:")
+print("test" == "test")
+
+print("5. 1==1 or 2 != 1 is:")
+print(1 == 1 or 2 != 1)
+
+print("6. True and 1 == 1 is:")
+print(True and 1 == 1)
+
+print("7. False and 0 != 0 is:")
+print(False and 0 != 0)
+
+print("8. True or 1 == 1 is:")
+print(True or 1 == 1)
+
+print('9. "test" == "testing" is:')
+print("test" == "testing")
+
+print("10. 1 != 0 and 2 == 1 is:")
+print(1 != 0 and 2 == 1)
+
+print('11. "test" != "testing" is:')
+print("test" != "testing")
+
+print('12. "test" == 1 is:')
+print("test" == 1)
+
+print("13. not (True and False) is:")
+print(not (True and False))
+
+print("14. not (1 == 1 and 0 != 1) is:")
+print(not (1 == 1 and 0 != 1))
+
+print("15. not (10 == 1 or 1000 == 1000 is:")
+print(not (10 == 1 or 1000 == 1000))
+
+print("16. not (1 != 10 or 3 == 4) is:")
+print(not (1 != 10 or 3 == 4))
+
+print('17. not ("testing" == "testing" and "Zed" == "Cool Guy" is:')
+print(not ("testing" == "testing" and "Zed" == "Cool Guy"))
+
+print('18. 1 == 1 and (not ("testing" == 1 or 1 == 0)) is:')
+print( 1 == 1 and (not ("testing" == 1 or 1 == 0 )))
+
+print('19. "chunky" == "bacon" and (not ( 3 == 4 or 3 == 3))')
+print("chunky" == "bacon" and (not (3 == 4 or 3 == 3)))
+
+print('20. 3 == 3 and (not ("testing" == "testing" or "Python" == "Fun"))')
+print(3 == 3 and (not ("testing" == "testing" or "Python" == "Fun")))
+
+
+
